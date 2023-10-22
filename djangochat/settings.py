f34ea5f5358c0e28704270252fd5875aca208c39
carbajaljerson <130612098+carbajaljerson@users.noreply.github.com>
2023-10-22 05:43:54 -0500

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+m+93dthq(9-9h008_756%-qg)y$r34501ga9x1cfkfwlil@9='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
